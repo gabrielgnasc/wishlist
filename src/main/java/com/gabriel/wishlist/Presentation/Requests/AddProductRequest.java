a024dc3ef51955ec00e7046b7ea702cc4e3932b5
Gabriel Nascimento <ggnascimento.98@gmail.com>
2025-05-10 14:06:07 -0300

    feat: Adição de Swagger para documentação das rotas

--- src/main/java/com/gabriel/wishlist/Presentation/Requests/AddProductRequest.java
+++ src/main/java/com/gabriel/wishlist/Presentation/Requests/AddProductRequest.java
@@ -1,5 +1,6 @@
 package com.gabriel.wishlist.Presentation.Requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,8 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Schema(name = "AddProductRequest", description = "Requisição para adicionar produto à wishlist")
 public class AddProductRequest {
 
     @NotBlank
+    @Schema(description = "ID do produto a ser adicionado", example = "67890", required = true)
     private String productId;
 }
